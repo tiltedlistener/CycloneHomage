85cc5ca6d0be9042d48da5da36b6f12a46d3d1ba
tiltedlistener <tiltedlistener@gmail.com>
2014-06-26 23:24:50 -0700

    Expanding on chapter 2, and dot, cross, normalization, support methods, easy arithmetic of vectors. Need to jUnit this stuff.

--- src/math/Vector3.java
+++ src/math/Vector3.java
@@ -48,4 +48,155 @@ public class Vector3 {
 		return x*x + y*y + z*z;
 	}
 	
+	/**
+	 * Get Direction
+	 * @return
+	 */
+	public double direction() {
+		// TODO
+		return 0;
+	}
+	
+	/**
+	 * Normalizes the vector
+	 */
+	public void normalize() {
+		double mag = this.magnitude();
+		if (mag > 0) {
+			this.x = this.x/mag;
+			this.y = this.y/mag;
+			this.y = this.z/mag;
+		}
+	}
+	
+	/**
+	 * Helpful to get normal vectors without altering the vector itself
+	 * @return computed normal or empty vector
+	 */
+	public Vector3 getNormal() {
+		double mag = this.magnitude();
+		if (mag > 0) {
+			return new Vector3(
+					this.x/mag,
+					this.y/mag,
+					this.z/mag
+					);
+		} else {
+			return new Vector3();
+		}
+	}
+	
+	/**
+	 * Adds a vector to this vector
+	 * @param addition
+	 */
+	public void addTo(Vector3 addition) {
+		this.x += addition.x;
+		this.y += addition.y;
+		this.z += addition.z;
+	}
+	
+	/**
+	 * Creates new vector after addition
+	 * @param addition
+	 * @return
+	 */
+	public Vector3 addAndCreate(Vector3 addition) {
+		return new Vector3(addition.x + this.x, addition.y + this.y, addition.z + this.z);
+	}
+	
+	/**
+	 * Subtracts a vector from this one. 
+	 * @param subtrahend
+	 */
+	public void difference(Vector3 subtrahend) {
+		this.x -= subtrahend.x;
+		this.y -= subtrahend.y;
+		this.z -= subtrahend.z;
+	}
+	
+	/**
+	 * Subtracts from this vector and creates a new one.
+	 * @param subtrahend
+	 * @return new Vector that's the result of the current operation
+	 */
+	public Vector3 differenceAndCreate(Vector3 subtrahend) {
+		return new Vector3(this.x - subtrahend.x, this.y - subtrahend.y, this.z - subtrahend.z);
+	}
+	
+	/**
+	 * Scalar operation
+	 * @param scalar to grow by
+	 */
+	public void scalar(double scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+		this.z *= scalar;
+	}
+	
+	/**
+	 * times, desired overload operator for Vec3 * Vec3
+	 * @param multiplicand
+	 * @return Multiplied vector
+	 */
+	public Vector3 times(Vector3 multiplicand) {
+		return new Vector3(multiplicand.x * this.x, multiplicand.y * this.y, multiplicand.z * this.z);
+	}
+	
+	/**
+	 * Result of the vector (or rather Cross) product
+	 * @param vector
+	 * @return new vector from the result of the operation
+	 */
+	public Vector3 vectorProduct(Vector3 vector) {
+		return new Vector3(
+				this.y * vector.z - this.z * vector.y, 
+				this.z * vector.x - this.x * vector.z,
+				this.x * vector.y - this.y * vector.x 
+		);
+	}
+	
+	/**
+	 * Helpful method to just get the vector product set to current vector
+	 * @param vector
+	 */
+	public void vectorProductToSelf(Vector3 vector) {
+		this.setAsSelf(this.vectorProduct(vector));
+	}
+	
+	/**
+	 * Computes the standard vector dot product
+	 * @param vec
+	 * @return 
+	 */
+	public double dotProduct(Vector3 vec) {
+		return this.x*vec.x + this.y*vec.y + this.z*vec.z;
+	}
+	
+	/**
+	 * Finds the orthonormal basis
+	 * @param a
+	 */
+	public void makeOrthonormalBasis(Vector3 a) {
+		this.normalize();
+		Vector3 b = new Vector3();
+		b.setAsSelf(this.vectorProduct(a));
+		if (b.squareMagnitude() == 0) return;
+		b.normalize();
+		a.setAsSelf(b.vectorProduct(this));
+	}
+	
+	/**
+	 * Support methods for copying, resetting
+	 */
+	public void setAsSelf(Vector3 vector) {
+		this.x = vector.x;
+		this.y = vector.y;
+		this.z = vector.z;
+	}
+	
+	public Vector3 clone() {
+		return new Vector3(this.x, this.y, this.z);
+	}
+	
 }
